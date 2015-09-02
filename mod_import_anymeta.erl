412819c1efc42b770cd531fb12f72eafc99aaea8
Marc Worrell <marc@worrell.nl>
2015-09-02 14:57:15 +0200

    Also catch problems with unknown predicates in query conversion.

--- mod_import_anymeta.erl
+++ mod_import_anymeta.erl
@@ -1229,6 +1229,11 @@ rsc_insert(Props, Context) ->
     try
         m_rsc_update:insert(Props, Options, Context)
     catch
+        error:{unknown_predicate,Pred} ->
+            lager:warning("[~p] Anymeta importer: Rescource has invalid query ~p, unknown predicate ~p.",
+                          [z_context:site(Context), proplists:lookup('query', Props), Pred]),
+            Props1 = proplists:delete('query', Props),
+            m_rsc_update:insert(Props1, Options, Context);
         throw:{error, invalid_query} ->
             lager:warning("[~p] Anymeta importer: Rescource has invalid query ~p, dropping query.",
                           [z_context:site(Context), proplists:lookup('query', Props)]),
